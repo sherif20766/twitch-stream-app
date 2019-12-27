0dbc2833e61c83f986c3589d1a0d5ee02c63a5a8
sherif20766 <sherif20766@hotmail.com>
2019-12-27 14:58:05 +0000

    initialized axios and redux thunk, added ability to create stream record with REST conventions using json-server

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
 	//	renderInput(formProps) {
@@ -31,9 +33,9 @@ class StreamCreate extends React.Component {
 		);
 	};
 
-	onSubmit(formValues) {
-		console.log(formValues);
-	}
+	onSubmit = formValues => {
+		this.props.createStream(formValues);
+	};
 
 	render() {
 		return (
@@ -70,7 +72,9 @@ const validate = formValues => {
 	return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
 	form: "streamCreate",
 	validate: validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
