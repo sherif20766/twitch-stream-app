64ee196ac954e86a869b42e203ecfcc574c7c4d3
sherif20766 <sherif20766@hotmail.com>
2019-12-29 16:26:28 +0000

    created renderCreate() helper function which renders a create stream button if user is signed in

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
@@ -35,11 +36,24 @@ class StreamList extends React.Component {
 		});
 	}
 
+	renderCreate() {
+		if (this.props.isSignedIn) {
+			return (
+				<div>
+					<Link to="/streams/new" className="ui button primary">
+						Create Stream
+					</Link>
+				</div>
+			);
+		}
+	}
+
 	render() {
 		return (
-			<div>
+			<div style={{ textAlign: "right" }}>
 				<h2>Streams</h2>
 				<div className="ui celled list">{this.renderList()}</div>
+				{this.renderCreate()}
 			</div>
 		);
 	}
@@ -48,7 +62,8 @@ class StreamList extends React.Component {
 const mapStateToProps = state => {
 	return {
 		streams: Object.values(state.streams),
-		currentUserId: state.auth.userId
+		currentUserId: state.auth.userId,
+		isSignedIn: state.auth.isSignedIn
 	};
 };
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
