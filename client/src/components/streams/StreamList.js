2e8c6d6331f57450d8b732f3dd44b6d7e788b19c
sherif20766 <sherif20766@hotmail.com>
2019-12-29 15:59:52 +0000

    conditionally render edit and delete stream buttons depending on whether user is logged in

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,10 +7,24 @@ class StreamList extends React.Component {
 		this.props.fetchStreams();
 	}
 
+	renderAdmin(stream) {
+		if (stream.userId === this.props.currentUserId) {
+			return (
+				<div>
+					<div className="right floated content">
+						<button className="ui button primary">Edit</button>
+						<button className="ui button negative">Delete</button>
+					</div>
+				</div>
+			);
+		}
+	}
+
 	renderList() {
 		return this.props.streams.map(stream => {
 			return (
 				<div className="item" key={stream.id}>
+					{this.renderAdmin(stream)}
 					<i className="large middle aligned icon camera" />
 					<div className="content">
 						{stream.title}
@@ -33,7 +47,8 @@ class StreamList extends React.Component {
 
 const mapStateToProps = state => {
 	return {
-		streams: Object.values(state.streams)
+		streams: Object.values(state.streams),
+		currentUserId: state.auth.userId
 	};
 };
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
