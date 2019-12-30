c56ba791ac486ec8dff8289d9a3ab21abb9e4e8f
sherif20766 <sherif20766@hotmail.com>
2019-12-30 14:45:05 +0000

    created a modal for /streams/delete using reactPortals

---
+++ client/src/components/Modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Modal = props => {
+	return ReactDOM.createPortal(
+		<div className="ui dimmer modals visible active">
+			<div className="ui standard modal visible active">TEST MODAL</div>
+		</div>,
+
+		document.querySelector("#modal")
+	);
+};
+
+export default Modal;
