e816cef6af1959de0b626d5d64a965838194a108
sherif20766 <sherif20766@hotmail.com>
2019-12-30 12:21:08 +0000

    changed fetchStream's request to PUT

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -52,7 +52,9 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-	const response = await streams.put(`/streams/${id}`, formValues);
+	// use PATCH (rather than PUT) request
+	// in order to update SOME properties of record
+	const response = await streams.patch(`/streams/${id}`, formValues);
 
 	dispatch({
 		type: EDIT_STREAM,
