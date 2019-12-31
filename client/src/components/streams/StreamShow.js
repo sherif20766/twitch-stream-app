cf13ba06b2d0b08bd2da33ebe58ce74dfcf615bc
sherif20766 <sherif20766@hotmail.com>
2019-12-31 19:39:39 +0000

    setup flv.js in order for streams to appear on the StreamShow component

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -9,9 +9,31 @@ class StreamShow extends React.Component {
 
 		this.videoRef = React.createRef();
 	}
+
 	componentDidMount() {
-		this.props.fetchStream(this.props.match.params.id);
+		const { id } = this.props.match.params;
+		this.props.fetchStream(id);
+		this.buildPlayer();
 	}
+	componentDidUpdate() {
+		this.buildPlayer();
+	}
+
+	buildPlayer() {
+		if (this.player || !this.props.stream) {
+			return;
+		}
+		const { id } = this.props.match.params;
+
+		this.player = flv.createPlayer({
+			type: "flv",
+			url: `http://localhost:8000/live/${id}.flv`
+		});
+
+		this.player.attachMediaElement(this.videoRef.current);
+		this.player.load();
+	}
+
 	render() {
 		if (!this.props.stream) {
 			return <div>Loading...</div>;
